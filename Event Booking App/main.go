3590250c4bcb79d5f1f5639dd827944fe3a4d47c
Vinod Pujari <pujarivinod0313@gmail.com>
2024-07-30 17:30:02 +0530

    Added event model, api-test for create & get events

--- Event Booking App/main.go
+++ Event Booking App/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"event-booking-app/models"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -10,10 +11,25 @@ func main() {
 	server := gin.Default()
 
 	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
 
 	server.Run(":8080")
 }
 
 func getEvents(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{"message": "Hello World"})
+	allEvents := models.GetAllEvents()
+	context.JSON(http.StatusOK, allEvents)
+}
+
+func createEvent(context *gin.Context) {
+	var event models.Event
+	err := context.ShouldBindBodyWithJSON(&event)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Something Went Wrong!"})
+		return
+	}
+	event.ID = 1
+	event.UserID = 1
+	event.Save()
+	context.JSON(http.StatusCreated, gin.H{"message": "Event Created!", "event": event})
 }
